7fcfc9cb41c3af30c296f5c617c8dec44cb8e096
fanghongbo <718787573@qq.com>
2026-03-18 15:42:04 +0800

    #1134572 update default workbench data

--- packages/effects/common-ui/src/ui/dashboard/workbench/workbench-header.vue
+++ packages/effects/common-ui/src/ui/dashboard/workbench/workbench-header.vue
@@ -30,16 +30,16 @@ withDefaults(defineProps<Props>(), {
     <div class="mt-4 flex flex-1 justify-end md:mt-0">
       <div class="flex flex-col justify-center text-right">
         <span class="text-foreground/80"> 待办 </span>
-        <span class="text-2xl">2/10</span>
+        <span class="text-2xl">0/0</span>
       </div>
 
       <div class="mx-12 flex flex-col justify-center text-right md:mx-16">
         <span class="text-foreground/80"> 项目 </span>
-        <span class="text-2xl">8</span>
+        <span class="text-2xl">0</span>
       </div>
       <div class="mr-4 flex flex-col justify-center text-right md:mr-10">
         <span class="text-foreground/80"> 团队 </span>
-        <span class="text-2xl">300</span>
+        <span class="text-2xl">0</span>
       </div>
     </div>
   </div>
