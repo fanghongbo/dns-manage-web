8307df2c7e398c011264d94631c53789e5517893
fanghongbo <718787573@qq.com>
2026-03-19 09:44:43 +0800

    #1134572 fix lock screen disable failed

--- packages/effects/layouts/src/basic/layout.vue
+++ packages/effects/layouts/src/basic/layout.vue
@@ -394,9 +394,9 @@ const headerSlots = computed(() => {
         :check-updates-interval="preferences.app.checkUpdatesInterval"
       />
 
-      <Transition v-if="preferences.widget.lockScreen" name="slide-up">
+      <!-- <Transition v-if="preferences.widget.lockScreen" name="slide-up">
         <slot v-if="accessStore.isLockScreen" name="lock-screen"></slot>
-      </Transition>
+      </Transition> -->
 
       <template v-if="preferencesButtonPosition.fixed">
         <Preferences
